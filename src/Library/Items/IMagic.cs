38c89874d97639c001ed1f6d77d59fca98c10c26
Facu <facupintos@hotmail.com>
2022-10-14 23:58:34 -0300

    Actualizacion en clase Imagic

--- src/Library/Items/IMagic.cs
+++ src/Library/Items/IMagic.cs
@@ -1,4 +1,14 @@
 namespace RoleplayGame{
 
-    
+    public interface IMagic
+    {
+
+        string type {get; set;}
+        int AttackValue {get; set;}
+        int DefenseValue {get; set;}
+
+        void UseMagic();  
+
+
+    }
 }
