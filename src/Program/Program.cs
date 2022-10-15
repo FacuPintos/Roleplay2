5ae5372d71d1c15c3cd6b74e9a341ca8849559fb
Tincho44 <martinharbuet@gmail.com>
2022-10-15 01:06:47 -0300

    Se sube correcion de muchas clases

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,9 +8,12 @@ namespace Program
         static void Main(string[] args)
         {
             Archer guillermo = new Archer ("Guillermo",0,0,100);
-            Bow arco = new Bow ("arco");
+            Bow arco = new Bow("arco");
+            guillermo.PickUpItem(arco);
             guillermo.EquipItems("arco");
             Console.WriteLine(guillermo.attack);
+
+            // Lo que hace esto es aumentar el ataque de guillermo que es un arquero en 15, dado que el poder de arquero es 15.
         }
     }
 }
